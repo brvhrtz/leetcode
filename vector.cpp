588bda73fd1c20d736a8c7922e65bedce3c85d8c
fj49ers <jameszhf@gmail.com>
2014-01-08 21:16:51 -0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -7,17 +7,20 @@
  *
  *
  */
- 
- 
-#include <iostream>
-#include <vector>
-
-using namespace std; 
-
 int main()
 {
-    vector<int> vec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+     // constructors used in the same order as described above:
+  std::vector<int> first;                                // empty vector of ints
+  std::vector<int> second (4,100);                       // four ints with value 100
+  std::vector<int> third (second.begin(),second.end());  // iterating through second
+  std::vector<int> fourth (third);                       // a copy of third
+
+  // the iterator constructor can also be used to construct from arrays:
+  int myints[] = {16,2,77,29};
+  std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
     
+    vector<int> vec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};  // C++11
     vec.erase(vec.begin() + 3); // erase 4th element
     vec.erase(vec.begin(), vec.begin() + 3); 
     // erase from begin() to begin() + 2 but not begin()+3 
@@ -41,11 +44,6 @@ int main()
  * 2. After deletion, the size of vector remains unchanged, each item now point to undefined address
  * 3. Do a "vec.clear()" to clear the vector itself. 
  */ 
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
 
 struct Node
 {
